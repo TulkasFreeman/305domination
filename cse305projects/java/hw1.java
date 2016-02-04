5e6d4ba50210a51e3b8071fce3a72e1e992ceb21
Vash1202 <hanklin@buffalo.edu>
2016-02-04 09:06:16 -0500

    Fleshed out more of hw1.java # # Changes to be committed: #	modified:   cse305projects/java/hw1.java #

--- cse305projects/java/hw1.java
+++ cse305projects/java/hw1.java
@@ -1,3 +1,19 @@
 /*For first assignment*/
 
 /*Set test cases, assign functionality to teammembers. Win*/
+
+public class hw1
+{
+    public static void hw1(inFile, outFile)
+    {
+        //function definition here
+    } 
+}
+
+public class tester
+{
+    public static void main(String[] args)
+    { 
+        System.out.println(new hw1().hw1(input,output));
+    }
+}
